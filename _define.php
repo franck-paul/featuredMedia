0bb5f37002c8abfec6e7f855d68206f3827bd39e
franck <carnet.franck.paul@gmail.com>
2020-02-14 14:20:27 +0100

    Prepare 0.2 release

--- _define.php
+++ _define.php
@@ -17,7 +17,7 @@ $this->registerModule(
     "Featured Media",                  // Name
     "Manage featured media for entry", // Description
     "Franck Paul",                     // Author
-    '0.1',                             // Version
+    '0.2',                             // Version
     [
         'requires'    => [['core', '2.13']],         // Dependencies
         'permissions' => 'usage,contentadmin,pages', // Permissions
