096f87ac50d39e1bc604647ee53dd92fdae94beb
franck <carnet.franck.paul@gmail.com>
2023-06-01 10:10:12 +0200

    l10n

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -11,10 +11,10 @@
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['featuredMedia'] = 'Média à la une';
+L10n::$locales['featuredMedia'] = 'featuredMedia';
 L10n::$locales['Are you sure you want to remove featured media "%s"?'] = 'Êtes-vous sûr·e de vouloir retirer le média à la une "%s" ?';
 L10n::$locales['Featured media'] = 'Média à la une';
-L10n::$locales['open'] = '';
+L10n::$locales['open'] = 'ouvrir';
 L10n::$locales['No featured media.'] = 'Aucun média à la une.';
 L10n::$locales['Add a featured media for this entry'] = 'Ajouter un média à la une à cette publication';
 L10n::$locales['Featured media:'] = 'Média à la une :';
