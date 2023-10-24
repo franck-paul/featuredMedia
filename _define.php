c91ade9a0e54b64e045df40e4e4739027ab2cc81
franck <carnet.franck.paul@gmail.com>
2023-10-24 18:49:19 +0200

    New release 4.0

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Featured Media',
     'Manage featured media for entry',
     'Franck Paul',
-    '3.0',
+    '4.0',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
